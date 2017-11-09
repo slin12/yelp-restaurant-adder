7008bb070a20f294071efc023d5f96a2e1d0933b
slin12 <s.lin12@gmail.com>
2017-11-09 13:58:00 -0500

    add validations

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# README
+# Restaurant Adder
 
 A simple rails add that shows:
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_many :user_restaurants
   has_many :restaurants, through: :user_restaurants
-  
   has_secure_password
+  validates :username, uniqueness: true, presence: true
+  validates :email, presence: true
+  validates :password, confirmation: true, presence: true
 
   def new
     @user = User.new
