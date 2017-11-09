2f3a5b37dad1d433983b829f250095ab9b0cd405
slin12 <s.lin12@gmail.com>
2017-11-09 14:07:36 -0500

    add flash notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: sessions_params[:username])
     if user && user.authenticate(sessions_params[:password])
       session[:user_id] = user.id
+      flash[:notice] = "You've successfully logged in. Welcome!"
       redirect_to restaurants_path
     else
       redirect_to login_path
@@ -14,6 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.destroy
+    flash[:notice] = "You've successfully logged out."
     redirect_to login_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:notice] = "You've successfully signed up"
       redirect_to '/restaurants'
     else
       redirect_to '/signup'
